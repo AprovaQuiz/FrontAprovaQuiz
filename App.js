a10d4708ee55600197d292f86eabfb1280b26caa
Giovanna B. dos Santos <giovanna.berato@gmail.com>
2024-04-02 21:08:00 -0300

    teste2

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#908',
     alignItems: 'center',
     justifyContent: 'center',
   },
